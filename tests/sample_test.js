ed34a14cc1c9c55e6bf8cc4c830431b2f961636b
smaranjitghose <46641503+smaranjitghose@users.noreply.github.com>
2022-12-31 06:18:05 -0500

    Added Performance Test

---
+++ tests/sample_test.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  vus: 1,
+  duration: '20s',
+  insecureSkipTLSVerify: true,
+  noConnectionReuse:false
+};
+
+export default function () {
+  http.get('http://localhost:8051');
+  sleep(2);
+}
